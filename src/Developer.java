94a14fa8276eb24c87ca60613dc81b41d20fc053
Tema <My_Post_Box@bk.ru>
2020-06-06 22:36:46 +0300

    Junior Android Developer

--- src/Developer.java
+++ src/Developer.java
@@ -1,30 +1,37 @@
+//The first task
 public class Developer {
     public static void main(String[] args) {
         User tester = new User(7,"Developer");
         System.out.println(tester.getId());
-        //System.out.println(tester.getVacancy());
         System.out.println(tester.getVacancy());
     }}
 class User {
     private long id;
     private Vacancy vacancy;
-    String getVacancy(String A) {
-        return vacancy.getVacancy(A);
+
+    String getVacancy() {
+        return vacancy.getVacancy();
     }
 
-        public User(long id,String vacancyMy) {
-            this.id = id;
-            this.vacancy = new Vacancy();
-            this.vacancy.getVacancy(vacancyMy);
-        }
-
-        long getId() {
-            return id;
-        }
-    }
-    class Vacancy {
-        String getVacancy(String A) {
-            return A;
-        }
+    public User(long id,String vacancyMy) {
+        this.id = id;
+        this.vacancy = new Vacancy(vacancyMy);
     }
 
+    long getId() {
+        return id;
+    }
+
+}
+class Vacancy {
+
+    private String vacancy;
+
+    public Vacancy(String vacancy) {
+        this.vacancy = vacancy;
+    }
+
+    String getVacancy() {
+        return vacancy;
+    }
+}
